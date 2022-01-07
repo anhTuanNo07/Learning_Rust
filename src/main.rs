225a254226c53a155188cbc6830c532940ebb122
Anh Tuan <you@example.com>
2022-01-07 16:21:29 +0700

    lesson 10

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,55 @@
-struct hinhChuNhat {
-    dai: u32, 
-    rong: u32,
+struct Data {
+    num1: i32,
+    num2: i32,
+    str1: String,
+    optional: Option<i32>,
+}
+
+struct Data2 {
+    num1: i32,
+    num2: i32,
+    str1: String,
+    optional: Option<i32>,
+}
+
+impl Data {
+    fn new() -> Self{
+        Data {
+            num1: 15,
+            num2: 25, 
+            str1: "some string 2".to_string(),
+            optional: None,
+        }
+    }
+}
+
+trait Transform {
+    fn revert(&self) -> String {
+        String::from("No string...")
+    }
+}
+
+impl Transform for Data {
+    fn revert(&self) -> String {
+        self.str1.chars().rev().collect::<String>()
+    }
+}
+
+impl Transform for Data2 {
+    fn revert(&self) -> String {
+        (self.num1 + self.num2).to_string()
+    }
 }
 
 fn main() {
-    let kichThuoc = hinhChuNhat {
-        dai: 50,
-        rong: 30,
+    let a = Data::new();
+
+    let b = Data2 {
+        num1: 10,
+        num2: 20,
+        str1: String::from("ok string"),
+        optional: None,
     };
-
-    println!("dien tich hinh chu nhat la {}", dien_tich(&kichThuoc))
-}
-
-fn dien_tich(kich_thuoc: &hinhChuNhat) -> u32 {
-    kich_thuoc.dai * kich_thuoc.rong
+    
+    println!("{}", a.revert());
 }
